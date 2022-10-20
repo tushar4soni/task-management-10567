18ae41c70f0f20c5d16c85caca2cb2852240d78a
tushar4soni <tushar4soni@gmail.com>
2022-10-21 01:08:57 +0530

    added two functions in savechanges button

--- scripts/index.js
+++ scripts/index.js
@@ -2,54 +2,62 @@ const state={
     tasklist:[]
 };
 
- const taskContents=document.querySelector(".task__contents");
- const taskModal=document.querySelector(".task__modal_body");
+ const taskContent=document.querySelector(".task__contents");
+ const taskModal=document.querySelector(".task__modal__body");
+  
+ 
 
 const htmlTaskContent=({id,title,description,type,url})=>
      ` <div class='col-md-6 col-lg-4 nt-3' id=${id} key=${id}>
        <div class='card shadow-sm task__card'>
        <div class='card-header gap-2 d-flex justify-content-end task__card__header'>
          <button type='button' class='btn btn-outline-info mr-2' name=${id}>
-         <i class='fas fa-pencil-alt' name=${id}></i> </button>
+            <i class='fas fa-pencil-alt' name=${id}></i>
+          </button>
 
          <button type="button" class='btn btn-outline-danger mr-2' name=${id}>
-         <i class='fas fa-trash-alt' name=${id}></i> </button>
-        </div>
-         <div class='card-body'>
-         ${
-            url && `<img width='100%' src=${url} alt='card image ap' 
-            class='card-image-top md-3 rounded-lg' />`
-         }
-         <h4 class='task__card_title'>${title}</h4>
-         <p class='description trim-3-lines text-muted' data-gram_editor='false'>
-         ${description}</p>
-         <div class='tags text-white d-flex flex-wrap'>
-           <span class='badge bg-primary m-1'>${type}</span>
-         
-         </div>
-         </div>
-         <div class='card-footer>
-         <button type='button' class='btn btn-outline-primary float-right'
-         data-bs-toogle='modal'
-         data-ba-target='#showTask'>open task </button>
+           <i class='fas fa-trash-alt' name=${id}></i>
+          </button>
+          </div>
+             <div class='card-body'>
+              ${
+                  url && `<img width='100%' src=${url} alt='card image ap' 
+                  class='card-image-top md-3 rounded-lg' />`
+              }
+              <h4 class='task__card__title'>${title}</h4>
+              <p class='description trim-3-lines text-muted' data-gram_editor='false'>
+              ${description}</p>
+              <div class='tags text-white d-flex flex-wrap'>
+                <span class='badge bg-primary m-1'>${type}</span>
+              
+              </div>
+            </div>
+              <div class='card-footer'>
+               <button type='button' class='btn btn-outline-primary float-right'
+                data-bs-toggle='modal'
+                 data-bs-target='#showTask'
+                 id=${id}
+                 onclick='openTask.apply(this,arguments)'
+                 >open task 
+                 </button>
 
-         </div>
+             </div>
 
-     </div>
+            </div>
      </div>
         
      `;
 
-     const htmlModalContent=({id,tittle,description,url}) =>{
+     const htmlModalContent=({id,title,description,url}) =>{
       const date =new Date(parseInt(id));
       return `
       <div id=${id}> 
       ${
-        url&& `<img width='100%' src=${url} alt='card image ap' 
+        url && `<img width='100%' src=${url} alt='card image ap' 
         class='img-fluid place__holder__image mb-3' />`
       }
-      <strong class='text-sm-muted'>Created on ${date.toDateString()}</strong>
-      <h2 class='my-3'>${tittle}</h2>
+      <strong class='text-sm text-muted'>Created on ${date.toDateString()}</strong>
+      <h2 class='my-3'>${title}</h2>
       <p class='Lead'>${description}</p>
 
       </div>    
@@ -58,7 +66,7 @@ const htmlTaskContent=({id,title,description,type,url})=>
 
      };
      const updateLocalStorage=()=>{
-      localStorage.setItem('tasks',JSON.stringify({ 
+      localStorage.setItem("tasks",JSON.stringify({ 
         tasks:state.tasklist,
       })
       );
@@ -71,7 +79,7 @@ const htmlTaskContent=({id,title,description,type,url})=>
       if(LocalStorageCopy) state.tasklist= LocalStorageCopy.tasks;
 
       state.tasklist.map((cardDate) =>{
-        taskContents.insertAdjacentHTML('beforeend',htmlTaskContent(cardDate));
+        taskContent.insertAdjacentHTML('beforeend',htmlTaskContent(cardDate));
 
 
       });
@@ -89,8 +97,13 @@ const htmlTaskContent=({id,title,description,type,url})=>
 
       };
 
-      taskContents.insertAdjecentHTML(
-        "beforeend",htmlTaskContent({
+      if(input.tittle===''|| input.description===''|| input.type ===''){
+        return alert("Please fill all the fields");
+      }
+
+      taskContent.insertAdjacentHTML(
+        "beforeend",
+        htmlTaskContent({
           ...input,
           id,
         })
@@ -100,6 +113,14 @@ const htmlTaskContent=({id,title,description,type,url})=>
       updateLocalStorage();
         
     };
+
+    const openTask=(e) =>{
+      if(!e) e :; window.event;
+
+     const getTask=state.tasklist.find(({id}) =>id===e.target.id);
+     taskModal.innerHTML=htmlModalContent(getTask);
+
+    }
 
 
      
